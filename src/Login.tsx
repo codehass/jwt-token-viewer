4f61949052115a664d7dbcc4ca7ebfb89ec0fd88
codehass <hassan.elouardy06@gmail.com>
2024-07-11 09:19:12 +0100

    create login form component

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,50 @@
+import React, { useState } from "react";
+import { login } from "./services/apiAuth";
+
 function Login() {
-	return <div>This is Login pqge</div>;
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		login({ username, password });
+		setUsername("");
+		setPassword("");
+	};
+
+	return (
+		<div>
+			<h1>Login Form</h1>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="username">Username:</label>
+					<input
+						type="text"
+						id="username"
+						placeholder="User Name"
+						value={username}
+						required
+						onChange={(e) => setUsername(e.target.value)}
+						//disabled={isLoading}
+					/>
+				</div>
+				<div>
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						id="password"
+						placeholder="Password"
+						value={password}
+						required
+						onChange={(e) => setPassword(e.target.value)}
+						//disabled={isLoading}
+					/>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
