094825ec03b75cc396832bffd7b13900d272c245
codehass <hassan.elouardy06@gmail.com>
2024-07-15 11:25:18 +0100

    use from to check the login in frontend

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import  { FormEvent,useState } from "react";
 import { login } from "./services/apiAuth";
 
 function Login() {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  // const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+  const handleSubmit = async (event:FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+    // setError('');
 
-		login({ username, password });
-		setUsername("");
-		setPassword("");
-	};
+    try {
+      const data = await login({ username, password });
+      console.log('Received data:', data);
+    } catch (error) {
+      console.log('Login failed. Please check your username and password.');
+      console.error('Error during login:', error);
+    } finally {
+      setIsLoading(false);
+    }
+
+		setUsername('');
+		setPassword('')
+	}
 
 	return (
 		<div>
@@ -26,7 +39,7 @@ function Login() {
 						value={username}
 						required
 						onChange={(e) => setUsername(e.target.value)}
-						//disabled={isLoading}
+						disabled={isLoading}
 					/>
 				</div>
 				<div>
@@ -38,10 +51,10 @@ function Login() {
 						value={password}
 						required
 						onChange={(e) => setPassword(e.target.value)}
-						//disabled={isLoading}
+						disabled={isLoading}
 					/>
 				</div>
-				<button type="submit">Submit</button>
+				<button type="submit" disabled={isLoading}>Submit</button>
 			</form>
 		</div>
 	);
