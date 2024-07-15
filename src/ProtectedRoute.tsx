547246ab33e18bd58b70d4f96658e909a48a0b01
codehass <hassan.elouardy06@gmail.com>
2024-07-15 22:50:50 +0100

    refactor protect route component

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,26 +1,23 @@
+// ProtectedRoute.tsx
 import { useNavigate, Outlet } from "react-router-dom";
 import { useEffect } from "react";
+import { useAuth } from "./AuthContext";
 
-function ProtectedRoute() {
-	const navigate = useNavigate();
+const ProtectedRoute: React.FC = () => {
+  const navigate = useNavigate();
+  const { isAuthenticated, isLoading } = useAuth();
 
-	// 1- Load the authenticated user
-	const isAuthenticated = true;
-	const isLoading = false;
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, isLoading, navigate]);
 
-	// 2- If there is no authenticated user, redirect to the login page
-	useEffect(() => {
-		if (!isAuthenticated && !isLoading) navigate("/login");
-	}, [isAuthenticated, isLoading, navigate]);
+  if (isLoading) return <div>Loading...</div>;
 
-	// 3- While loading show Spinner
-	if (isLoading) return <div>...</div>;
+  if (isAuthenticated) return <Outlet />;
 
-	// 4- If there is a user, render the app
-	if (isAuthenticated) return <Outlet />;
-
-	// 5- If not authenticated, you might want to return null or some fallback
-	return null;
-}
+  return null;
+};
 
 export default ProtectedRoute;
