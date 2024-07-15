1e64c7cccb95373471747372cabd401141392fa2
codehass <hassan.elouardy06@gmail.com>
2024-07-15 11:24:51 +0100

    use axios to fetch data from backend

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,15 +1,30 @@
+import axios from 'axios';
 interface LoginParams {
-	username: string;
-	password: string;
+  username: string;
+  password: string;
 }
 
-export async function login({
-	username,
-	password,
-}: LoginParams): Promise<void> {
-	console.log("Username:", username);
-	console.log("Password:", password);
+export async function login({ username, password }: LoginParams): Promise<void> {
+
+   const { data}=await axios.post('http://localhost:3000/login', {
+      username,
+      password
+    })
+
+  console.log(data)
+  return  data;
+
+  //   if (response.status === 200) {
+  //     console.log('Login successful');
+  //   } else {
+  //     console.log('Login failed');
+  //   }
+  // } catch (error) {
+  //   console.log('Login failed', error);
+  // }
 }
+
+
 
 export async function logout() {
 	console.log("logout");
