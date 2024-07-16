eda51d5bc40e577023cc190b5862485ef15f5e6f
codehass <hassan.elouardy06@gmail.com>
2024-07-16 19:59:50 +0100

    add headers

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
-import { useAuth } from './AuthContext'; // Adjust the import as necessary
+import { useAuth } from './AuthContext';
 
-// const url:string = 'http://localhost:3000';
-const url:string = 'https://master.dbzjdeaojpr79.amplifyapp.com';
+const url: string = 'https://master.dbzjdeaojpr79.amplifyapp.com';
+// const url: string = 'http://localhost:3000';
 
 const Home = () => {
-const { isAuthenticated, isLoading,logout } = useAuth();
-const [tokenInfo, setTokenInfo] = useState(null);
+  const { isAuthenticated, isLoading, logout } = useAuth();
+  const [tokenInfo, setTokenInfo] = useState(null);
 
   useEffect(() => {
     const fetchTokenInfo = async () => {
@@ -14,13 +14,16 @@ const [tokenInfo, setTokenInfo] = useState(null);
         const response = await fetch(`${url}/token_info`, {
           method: 'GET',
           credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
         });
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
         console.log(data);
-        setTokenInfo(data.data); 
+        setTokenInfo(data.data);
       } catch (error) {
         console.error('Error fetching token info:', error);
       }
@@ -35,9 +38,8 @@ const [tokenInfo, setTokenInfo] = useState(null);
     return <div>Loading...</div>;
   }
 
-    const handleLogout = async () => {
+  const handleLogout = async () => {
     await logout();
-    // Navigate is handled in the context, so no need to navigate here
   };
 
   return (
@@ -47,12 +49,12 @@ const [tokenInfo, setTokenInfo] = useState(null);
       </button>
       <h1>Your Component</h1>
       {tokenInfo ? (
-      Object.keys(tokenInfo).map((key, index) => (
-        <div key={index} style={{display: 'flex', alignItems:'center'}}> 
-          <div style={{fontWeight: 'bold', margin: '10px 0'}}>{key}</div> : {tokenInfo[key]}
-        </div>
-      ))
-    ) : (
+        Object.keys(tokenInfo).map((key, index) => (
+          <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ fontWeight: 'bold', margin: '10px 0' }}>{key}</div> : {tokenInfo[key]}
+          </div>
+        ))
+      ) : (
         <div>No token info available</div>
       )}
     </div>
