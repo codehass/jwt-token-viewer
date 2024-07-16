a51f2f9baaad4736b40ccbdf8a92910d37c6765b
codehass <hassan.elouardy06@gmail.com>
2024-07-16 18:23:20 +0100

    change the url

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -13,8 +13,8 @@ interface LoginParams {
   password: string;
 }
 
-const url:string = 'https://master.dbzjdeaojpr79.amplifyapp.com';
-// const url:string = 'http://localhost:3000'
+// const url:string = 'https://master.dbzjdeaojpr79.amplifyapp.com';
+const url:string = 'http://localhost:3000'
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
