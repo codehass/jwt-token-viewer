1db9efb57368bf60e6ba7eaa46b73cc59cd47079
codehass <hassan.elouardy06@gmail.com>
2024-07-17 02:05:33 +0100

    change url to amplify deployed link

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,8 @@ import { useAuth } from './AuthContext';
 
 
 // const url: string = 'http://localhost:3000';
-const url: string = 'https://hono-proxy-backend.onrender.com';
+// const url: string = 'https://hono-proxy-backend.onrender.com';
+const url: string = 'https://master.dbzjdeaojpr79.amplifyapp.com';
 
 const Home = () => {
   const { isAuthenticated, isLoading, logout } = useAuth();
